29ee3a9c5d7c6de46314bd88f0a85650bf23985e
Matthew Soucoup <msoucoup@codemilltech.com>
2016-09-11 18:58:06 -0500

    Initial add of all boilerplate

---
+++ src/Thunderbot.Weather/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thunderbot.Weather
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
